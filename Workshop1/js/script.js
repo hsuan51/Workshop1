fa009df3ec21e2f55513369d22098f06595c32ea
YI-HSUAN WU <wendy87040972@gamil.com>
2019-04-10 03:28:21 +0800

    Add book form kendo window.

--- Workshop1/js/script.js
+++ Workshop1/js/script.js
@@ -18,7 +18,8 @@ function loadBookData() {
     }
 }
 
-function showDeliveredDate(BookId,DeliveredDate) {
+// 使用tooltip顯示送達日期
+function showDeliveredDate(BookId, DeliveredDate) {
     //console.log(typeof DeliveredDate);
     $('#DeliveredIcon' + BookId).kendoTooltip({
         content: DeliveredDate
@@ -28,7 +29,7 @@ function showDeliveredDate(BookId,DeliveredDate) {
 function loadBookGrid() {
     $(document).ready(function () {
         var bookData = JSON.parse(localStorage.getItem('bookData'));
-        console.log(bookData);
+        //console.log(bookData);
         $("#book_grid").kendoGrid({
             toolbar: "<input type='search' class='k-textbox' id='searchBox' placeholder='我想要找..'>",
             dataSource: { data: bookData },
@@ -51,7 +52,7 @@ function loadBookGrid() {
                         var tr = $(e.target).closest("tr"); // get the current table row (tr)
                         // get the data bound to the current table row
                         var dataItem = this.dataItem(tr);
-                        kendo.confirm("確定刪除「" + dataItem.BookName + "」嗎?")
+                        kendo.confirm("確定刪除「" + dataItem.BookName + "」嗎?") //彈跳確認刪除視窗
                             .done(function () {
                                 var GridData = $("#book_grid").data("kendoGrid").dataSource;
                                 //console.log(GridData);
@@ -96,7 +97,7 @@ function loadBookGrid() {
             }, {
                 field: "BookDeliveredDate",
                 title: "送達狀態",
-                template: function (dataItem) {
+                template: function (dataItem) {//設定送達狀態icon
                     if (typeof dataItem.BookDeliveredDate != "undefined") {
                         //console.log(dataItem);
                         var BookId = dataItem.BookId;
@@ -149,7 +150,131 @@ function loadBookGrid() {
         });
     });
 }
+
+//新增書籍視窗
+function add_book_view() {
+    // 按下新增書籍跳出視窗
+    $(document).ready(function () {
+        $("#book_form").kendoWindow({
+            width: "600px",
+            height: "600px",
+            title: "新增書籍",
+            visible: false,
+            actions: [
+                "Pin",
+                "Minimize",
+                "Maximize",
+                "Close"
+            ],
+            close: onClose
+        }).data("kendoWindow").center();
+        //設置購買欲送達日期格式
+        $("#bought_datepicker").kendoDatePicker({
+            culture: "zh-TW",
+            value: new Date(),
+            format: "yyyy-MM-dd",
+            parseFormats: ["yyyy/MM/dd", "yyyyMMdd", "yyyy-MM-dd"]
+        });
+        $("#delivered_datepicker").kendoDatePicker({
+            culture: "zh-TW",
+            format: "yyyy-MM-dd",
+            parseFormats: ["yyyy/MM/dd", "yyyyMMdd", "yyyy-MM-dd"]
+        });
+        //選擇書的下拉式選單及更改圖片
+        $("#book_category").kendoDropDownList({
+            dataSource: bookCategoryList,
+            dataTextField: 'text',
+            dataValueField: 'value',
+            change: function () {
+                for (var x = 0; x < bookCategoryList.length; x++) {
+                    if ($("#book_category").val() == bookCategoryList[x]["value"]) {
+                        var imsrc = bookCategoryList[x]["src"];
+                        $(".book-image").attr("src", imsrc);
+                    }
+                }
+            }
+        });
+        //總計隨金額數量變動
+        $("#book_price,#book_amount").kendoNumericTextBox({
+            min: 0,
+            decimals: 0,
+            format: "{0:n0}",
+            change: function () {
+                var bookPrice = $("#book_price").val();
+                var bookAmount = $("#book_amount").val();
+                if (bookPrice != "" && bookAmount != "") {
+                    var total = parseInt(bookPrice) * parseInt(bookAmount);
+                    console.log(typeof total);
+                    document.getElementById("book_total").innerHTML = moneyFormat(total.toString());
+                } else {
+                    document.getElementById("book_total").innerHTML = 0;
+                }
+            }
+        });
+        //新增書籍至localstorage
+        $("#save_book").click(function () {
+            bookDataFromLocalStorage = JSON.parse(localStorage.getItem('bookData'));
+            bookid = bookDataFromLocalStorage[bookDataFromLocalStorage.length - 1]['BookId'] + 1;
+            BookCategory = document.getElementById("book_category").value;
+            BookName = document.getElementById("book_name").value;
+            BookAuthor = document.getElementById("book_author").value;
+            BookBoughtDate = document.getElementById("bought_datepicker").value;
+            BookDeliveredDate = document.getElementById("delivered_datepicker").value;
+            BookPrice = document.getElementById("book_price").value;
+            BookAmount = document.getElementById("book_amount").value;
+            BookTotal = BookPrice * BookAmount;
+            if (BookDeliveredDate.indexOf('-') == -1) {
+                bookDataFromLocalStorage.push({
+                    "BookId": bookid,
+                    "BookCategory": BookCategory,
+                    "BookName": BookName,
+                    "BookAuthor": BookAuthor,
+                    "BookBoughtDate": BookBoughtDate,
+                    "BookPublisher": "公司",
+                    "BookPrice": BookPrice,
+                    "BookAmount": BookAmount,
+                    "BookTotal": BookTotal
+                });
+            }
+            else {
+                bookDataFromLocalStorage.push({
+                    "BookId": bookid,
+                    "BookCategory": BookCategory,
+                    "BookName": BookName,
+                    "BookAuthor": BookAuthor,
+                    "BookBoughtDate": BookBoughtDate,
+                    "BookDeliveredDate": BookDeliveredDate,
+                    "BookPublisher": "公司",
+                    "BookPrice": BookPrice,
+                    "BookAmount": BookAmount,
+                    "BookTotal": BookTotal
+                });
+            }
+            localStorage.setItem('bookData', JSON.stringify(bookDataFromLocalStorage));
+            location.reload();
+            alert("新增成功");
+        });
+        $("#add_book").click(function () {
+            $("#book_form").data("kendoWindow").open();
+            $("#add_book").fadeOut();
+        });
+    });
+    
+    function onClose() {
+        $("#add_book").fadeIn();
+    }
+    
+}
+function moneyFormat(str) {
+    if (str.length <= 3) {
+        return str;
+    }
+    else {
+        return moneyFormat(str.substr(0, str.length - 3)) + "," + (str.substr(str.length - 3));
+    }
+}
 $(function () {
     loadBookData();
     loadBookGrid();
+    add_book_view();
 });
